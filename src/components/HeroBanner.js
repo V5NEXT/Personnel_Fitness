146764aa74fde1e4d2524c97ff5fcb96bef9b845
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-24 23:44:39 +0200

    Adding the Hero Component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,7 +3,7 @@ import {Box, Stack, Typography} from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box sx={{mt:{lg: '212px', xs:'70px'}, ml:{sm: '50px'}}}>
       <Typography color="#ff2625" fontWeight="600" fontSize="25px">
         My Fitness
       </Typography>
