1630f743c92765defebfcb79997c637e6c65ebb3
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-27 22:10:52 +0200

    Intermediate Commit

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Box, Typography, Stack, Textfield, Button } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center"
+    p="20px">
+      <Typography fontWeight={700} sx={{
+        fontSie:{lg: '44px', xs:'30px'}
+      }} mb="50px" textAlign="center">
+        Carefully follow and <br/>
+        Personalize yours own Excercises
+      </Typography>
+    </Stack>
   )
 }
 
