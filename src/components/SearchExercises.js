a46cd98d56aee282d1358e7a6674bb125cdd86c3
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-27 22:23:57 +0200

    Added Search Options

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Box, Typography, Stack, Textfield, Button } from '@mui/material';
+import { Box, Typography, Stack, Button, TextField } from '@mui/material';
 
 const SearchExercises = () => {
   return (
@@ -11,6 +11,17 @@ const SearchExercises = () => {
         Carefully follow and <br/>
         Personalize yours own Excercises
       </Typography>
+      <Box position="relative" mb="72px">
+        <TextField 
+        sx={{input: {fontWeight:'700', border:'none', borderRadius:'4px'},
+      width : {lg:'1170px', sm:'100%'}
+      }}
+        height="76px" value="" onChange={(e)=>{}}
+        placeholder="Search Exercises"
+        type="text"
+        />
+
+      </Box>
     </Stack>
   )
 }
